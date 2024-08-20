d8b9b241da0ca3f872850881c89687ce98ecaa70
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2024-08-20 10:39:13 +0300

    android server-gatt  iyl 2024

--- app/src/main/java/com/sous/server/businesslayer/Errors/SubClassErrors.java
+++ app/src/main/java/com/sous/server/businesslayer/Errors/SubClassErrors.java
@@ -8,6 +8,8 @@ import android.net.Uri;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.loader.content.AsyncTaskLoader;
 
 import com.sous.server.businesslayer.ContentProvoders.ContentProviderServer;
 
@@ -21,19 +23,44 @@ public class SubClassErrors {
     }
     public  void МетодЗаписиОшибок(@NonNull   ContentValues contentValuesДляЗаписиОшибки) {
         try {
-            Log.i( context.getClass().getName(), "contentValuesДляЗаписиОшибки  " + contentValuesДляЗаписиОшибки);
-            Integer getVersionforErrorNew=        getVersionforErrorNew("SELECT MAX ( current_table  ) AS MAX_R  FROM errordsu1");
-            contentValuesДляЗаписиОшибки.put("current_table",getVersionforErrorNew);
 
-            Uri uri = Uri.parse("content://com.sous.server.providerserver/errordsu1" );
-            ContentResolver resolver = context. getContentResolver();
-        Uri    insertData=   resolver.insert(uri, contentValuesДляЗаписиОшибки);
-            Integer РезультатВставки= Optional.ofNullable(insertData.toString().replaceAll("content://","")).map(Integer::new).orElse(0);
+            AsyncTaskLoader asyncTaskLoaderErrorWriter=new AsyncTaskLoader(context) {
+                @Nullable
+                @Override
+                public Object loadInBackground() {
+
+                    Log.i( context.getClass().getName(), "contentValuesДляЗаписиОшибки  " + contentValuesДляЗаписиОшибки);
+                    Integer getVersionforErrorNew=        getVersionforErrorNew("SELECT MAX ( current_table  ) AS MAX_R  FROM errordsu1");
+                    contentValuesДляЗаписиОшибки.put("current_table",getVersionforErrorNew);
+
+
+                    Integer getVersionUUID=        getVersionforErrorNew("SELECT MAX ( uuid  ) AS MAX_R  FROM errordsu1");
+                    contentValuesДляЗаписиОшибки.put("uuid",getVersionUUID);
+
+
+
+                    Uri uri = Uri.parse("content://com.sous.server.providerserver/errordsu1" );
+                    ContentResolver resolver = context. getContentResolver();
+                    Uri    insertData=   resolver.insert(uri, contentValuesДляЗаписиОшибки);
+                    Integer РезультатВставки= Optional.ofNullable(insertData.toString().replaceAll("content://","")).map(Integer::new).orElse(0);
+                    // TODO: 20.08.2024
+                    // TODO: 08.08.2024
+                    Log.d(this.getClass().getName(),"\n" + " class " + Thread.currentThread().getStackTrace()[2].getClassName() + "\n" +
+                            " metod " + Thread.currentThread().getStackTrace()[2].getMethodName() + "\n" +
+                            " line " + Thread.currentThread().getStackTrace()[2].getLineNumber() + "\n" +"ERROR ERROR ERROR GATT SERVER  !!!!!!"
+                            +"\n"+ " РезультатВставки " +РезультатВставки);
+                    return РезультатВставки;
+                }
+            };
+            // TODO: 20.08.2024
+
+            asyncTaskLoaderErrorWriter.forceLoad();
+            asyncTaskLoaderErrorWriter.loadInBackground();
+
             // TODO: 08.08.2024
             Log.d(this.getClass().getName(),"\n" + " class " + Thread.currentThread().getStackTrace()[2].getClassName() + "\n" +
                     " metod " + Thread.currentThread().getStackTrace()[2].getMethodName() + "\n" +
-                    " line " + Thread.currentThread().getStackTrace()[2].getLineNumber() + "\n" +"ERROR ERROR ERROR GATT SERVER  !!!!!!"
-                    +"\n"+ " РезультатВставки " +РезультатВставки);
+                    " line " + Thread.currentThread().getStackTrace()[2].getLineNumber() + "\n" +"ERROR ERROR ERROR GATT SERVER  !!!!!!" +"\n");
         } catch (Exception e) {
             e.printStackTrace();
             Log.e( context.getClass().getName(), "SubClassErrors ДЛЯ SCANNER error " + e +
