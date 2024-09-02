a43de6a2bb3f2bd555e2a517aa4b861dcfcfc17a
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2024-09-02 17:24:55 +0300

    android gatt client abgyst 2024

--- app/src/main/java/com/sous/scanner/businesslayer/bl_Tests/GetBleAdvertising.java
+++ app/src/main/java/com/sous/scanner/businesslayer/bl_Tests/GetBleAdvertising.java
@@ -72,7 +72,7 @@ public class GetBleAdvertising {
                     .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY)
                     .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_HIGH)
                     .setConnectable(true)
-                    .setTimeout(1000)
+                    .setTimeout(0)
                     .setTxPowerLevel(AdvertisingSetParameters.TX_POWER_HIGH)
                     .build();
 
