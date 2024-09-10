9fef450e5cf329c8656eddb3a3ab73d9a74d90d2
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2024-09-10 08:20:45 +0300

    android server-gatt  iyl 2024

--- app/src/main/java/com/sous/server/businesslayer/bl_Tests/GetBleAdvertising.java
+++ app/src/main/java/com/sous/server/businesslayer/bl_Tests/GetBleAdvertising.java
@@ -1,5 +1,6 @@
 package com.sous.server.businesslayer.bl_Tests;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.le.AdvertiseCallback;
@@ -16,6 +17,7 @@ import android.provider.Settings;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 
 
 import com.serverscan.datasync.businesslayer.Errors.SubClassErrors;
@@ -115,6 +117,16 @@ public class GetBleAdvertising {
             };
 
             if (   bluetoothAdapter!=null) {
+                if (ActivityCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
                 bluetoothAdapter.setName(ANDROID_NAME);
                 bluetoothAdapter.getBluetoothLeAdvertiser().startAdvertising(settings, advertiseData, mAdvertiseCallback);
             }
