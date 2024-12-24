217145a2b787ec9de531b077bd6038880951b0b4
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2024-12-24 14:17:31 +0300

    oktabry 2024 tabel

--- app/src/main/java/com/dsy/dsu/Passwords/MainActivityPasswords.java
+++ app/src/main/java/com/dsy/dsu/Passwords/MainActivityPasswords.java
@@ -29,6 +29,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import com.dsu.backgroundoperation.BackgroundGet;
 import com.dsy.dsu.AllDatabases.SQLTE.GetSQLiteDatabase;
 import com.dsy.dsu.BootAndAsync.EventsBus.MessageEvensBusAyns;
 import com.dsy.dsu.BootAndAsync.EventsBus.MessageEvensBusUpdatePO;
@@ -193,6 +194,11 @@ public class MainActivityPasswords extends AppCompatActivity {
             Log.d(this.getClass().getName(), "\n" + " class " + Thread.currentThread().getStackTrace()[2].getClassName() + "\n" +
                     " metod " + Thread.currentThread().getStackTrace()[2].getMethodName() + "\n" +
                     " line " + Thread.currentThread().getStackTrace()[2].getLineNumber() + "\n");
+
+
+           BackgroundGet backgroundGet=new BackgroundGet();
+            backgroundGet.backgroundGet();;
+
         } catch (Exception e) {
             ПрогрессБарДляВходаСистему.setVisibility(View.INVISIBLE);// при нажатии делаем видимый програсссбар
             Log.e(this.getClass().getName(), "Ошибка " + e + " Метод :" + Thread.currentThread().getStackTrace()[2].getMethodName() +
