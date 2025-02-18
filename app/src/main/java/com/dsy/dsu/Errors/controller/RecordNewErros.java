512e6fafd1ed3671dafdc1046d577c1a1e6a3421
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2025-02-18 15:37:08 +0300

    yanvary last 2025 tabel

--- app/src/main/java/com/dsy/dsu/Errors/controller/RecordNewErros.java
+++ app/src/main/java/com/dsy/dsu/Errors/controller/RecordNewErros.java
@@ -168,10 +168,10 @@ public class RecordNewErros  implements RecordNewErrorsInterface {
 
             Integer getDeviceVersion    =new ModulegetDeviceName().getDeviceVersion(context);
 
-            String getDeviceName    =new ModulegetDeviceName().getDeviceName(context);
+            String ANDROID_ID    =new ModulegetDeviceName().getDeviceName(context);
             // TODO: 30.01.2025 ВСТАВКА  новой ошибки
 
-            contentValuesNewError.put("Error", ТекстОшибки.toLowerCase() +"\n"+" Current Device: "+getDeviceName +"\n");
+            contentValuesNewError.put("Error", ТекстОшибки.toLowerCase() +"\n"+" Current Device: "+ANDROID_ID +"\n");
             contentValuesNewError.put("Klass", КлассГнерацииОшибки.toUpperCase());
             contentValuesNewError.put("Metod", МетодаОшибки.toUpperCase());
             contentValuesNewError.put("LineError", ЛинияОшибки);
