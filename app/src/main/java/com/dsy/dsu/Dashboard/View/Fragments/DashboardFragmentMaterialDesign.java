a28ae4e48f10c3dd7bcccc4e51118466745a449f
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2025-04-08 23:42:18 +0300

    march last 2025 tabel

--- app/src/main/java/com/dsy/dsu/Dashboard/View/Fragments/DashboardFragmentMaterialDesign.java
+++ app/src/main/java/com/dsy/dsu/Dashboard/View/Fragments/DashboardFragmentMaterialDesign.java
@@ -437,11 +437,11 @@ public class DashboardFragmentMaterialDesign extends  DialogFragment  {
                             Bundle data = new Bundle();
                             Интент_ЗапускТабельногоУчётаПервыйШаг.putExtras(data);
                             Интент_ЗапускТабельногоУчётаПервыйШаг.setClass(getContext(), MainActivity_List_Tabels.class); //  ТЕСТ КОД КОТОРЫЙ ЗАПУСКАЕТ ACTIVITY VIEWDATA  ПРОВЕРИТЬ ОБМЕН
-                            Интент_ЗапускТабельногоУчётаПервыйШаг.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            Интент_ЗапускТабельногоУчётаПервыйШаг.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
                             Log.d(this.getClass().getName(), "" + "    КнопкаТабельныйУчёт.setOnClickListener(new View.OnClickListener() {");
                             startActivity(Интент_ЗапускТабельногоУчётаПервыйШаг);
                             // TODO: 10.03.2025
-                            getActivity().finish();
+                            getActivity().finishAffinity();
                         } catch (Exception e) {
                             e.printStackTrace();
                             Log.e(this.getClass().getName(), "Ошибка " + e + " Метод :" + Thread.currentThread().getStackTrace()[2].getMethodName() + " Линия  :"
