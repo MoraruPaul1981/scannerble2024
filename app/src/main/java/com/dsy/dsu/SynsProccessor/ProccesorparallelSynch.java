0c2b612ee18d603b6e6fc61b1c400fd4a1613af6
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2025-06-04 17:13:07 +0300

    iyn 2025   AMD RYZEN 5600

--- app/src/main/java/com/dsy/dsu/SynsProccessor/ProccesorparallelSynch.java
+++ app/src/main/java/com/dsy/dsu/SynsProccessor/ProccesorparallelSynch.java
@@ -100,7 +100,7 @@ public class ProccesorparallelSynch   {
 // TODO: 20.01.2025 сама синхрониаиця
             case  "СамыйПервыйЗапускСинхронизации":
                     Flowable.fromIterable(getBufferFromJbossServerAllTables)
-                            .parallel().runOn(Schedulers.from(Executors.newFixedThreadPool(2)))
+                            .parallel().runOn(Schedulers.io())
                             .doOnNext(new Consumer<ConcurrentHashMap<String, String>>() {
                                 @Override
                                 public void accept(ConcurrentHashMap<String, String> operationMulti) throws Throwable {
